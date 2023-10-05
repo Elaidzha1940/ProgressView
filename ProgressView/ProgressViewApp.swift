d2ac834560523fb2106915a0cb7e94497c2a17c7
Elaidzha1940 <eli080293@gmail.com>
2023-10-05 03:36:21 +0300

    ProgressView

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Elaidzha Shchukin on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
